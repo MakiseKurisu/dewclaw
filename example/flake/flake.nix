aeedc57967ad49fac601b31e313cd7329c8db9c1
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2024-07-24 10:05:03 +0800

    docs: add flake example

---
+++ example/flake/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "dewclaw flake example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    dewclaw.url = "github:MakiseKurisu/dewclaw";
+  };
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , dewclaw
+    , ...
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          dewclaw-env = pkgs.callPackage dewclaw {
+            configuration = import ../classic/example.nix;
+          };
+          default = self.packages.x86_64-linux.dewclaw-env;
+        };
+      };
+    };
+}
