fe301a19c2e5f52962892afae0cc9a778b0cec30
Bjørn Forsman <bjorn.forsman@gmail.com>
2025-12-19 20:55:12 +0100

    Add OpenWRT 25.12 support
    The latest OpenWRT release replaced the opkg package manager with apk.
    Add support for apk while keeping opkg support for backward
    compatibility with OpenWRT 24.12.
    
    Implementation notes:
    
    * apk doesn't like dots in package names. Since the word "deps" is used
      a lot in the Nix and sh code I changed the OpenWRT package name from
      ".extra-system-deps." to "dewclaw-deps".
    * To create the apk package file I'm using `apk mkpkg`. That's a newer
      command, but much simpler to use than `abuild`.
    
    TODO: The current package is unsigned. Should we sign it?
    TODO: The apk code path doesn't remove orphaned packages, so this makes
    the dewclaw pakcages option not fully declarative.

--- openwrt/packages.nix
+++ openwrt/packages.nix
@@ -6,7 +6,27 @@
 }:
 
 let
-  deps = config.build.depsPackage;
+  package_name = "dewclaw-deps";
+  # apk rejects hash as version number, due to the letters. Convert them to
+  # digits.
+  version = let
+    hash = builtins.hashString "sha256" (toString config.packages);
+    digitsOnly =
+      lib.stringAsChars
+        (x:
+          if x == "a" then "10"
+          else if x == "b" then "11"
+          else if x == "c" then "12"
+          else if x == "d" then "13"
+          else if x == "e" then "14"
+          else if x == "f" then "15"
+          else x
+        )
+        hash;
+    in
+      digitsOnly;
+  depsApk = config.build.depsPackageApk;
+  depsIpk = config.build.depsPackageIpk;
 in
 
 {
@@ -15,34 +35,76 @@ in
     default = [ ];
     description = ''
       Extra packages to install. These are merely names of packages available
-      to opkg through the package source lists configured on the device, it is
-      not currently possible to provide packages for installation without
-      configuring an opkg source first.
+      for package manager through the package source lists configured on the
+      device, it is not currently possible to provide packages for installation
+      without configuring a package source first.
+
+      For backward compatibility with OpenWRT <= 24.10, opkg will be used if apk
+      is unavailable.
     '';
   };
 
   config = {
+    assertions = [
+      {
+        assertion = lib.versionAtLeast pkgs.apk-tools.version "3.0.0";
+        message = "apk-tools >= 3.0.0 is required to build APK packages (found ${pkgs.apk-tools.version}).";
+      }
+    ];
+
     deploySteps.packages = {
       priority = 80;
       copy = ''
-        scp ${deps} device:/tmp/deps-${deps.version}.ipk
+        scp ${depsApk} device:/tmp/deps-${version}.apk
+        scp ${depsIpk} device:/tmp/deps-${version}.ipk
       '';
       apply = ''
-        if [ ${deps.version} != "$(opkg info ${deps.package_name} | grep Version | cut -d' ' -f2)" ]; then
-          opkg update
-          opkg install --autoremove --force-downgrade /tmp/deps-${deps.version}.ipk
+        if command -v apk >/dev/null; then
+          if [ "${version}" != "$(apk list --installed --manifest "${package_name}" | cut -d' ' -f2)" ]; then
+            apk update
+            # TODO: sign packages?
+            # TODO: remove old packages that were previously pulled in as dependencies, but no longer needed.
+            apk add --allow-untrusted /tmp/deps-${version}.apk
+          fi
+        elif command -v opkg >/dev/null; then
+          if [ "${version}" != "$(opkg info ${package_name} | grep Version | cut -d' ' -f2)" ]; then
+            opkg update
+            opkg install --autoremove --force-downgrade /tmp/deps-${version}.ipk
+          fi
+        else
+          echo "error: missing package manager (tried 'apk' and 'opkg')"
+          return 1
         fi
+        rm /tmp/deps-${version}.apk
+        rm /tmp/deps-${version}.ipk
       '';
     };
 
-    build.depsPackage =
+    build.depsPackageApk =
+      pkgs.runCommand "deps.apk"
+        {
+          nativeBuildInputs = [
+            pkgs.apk-tools
+          ];
+        }
+        ''
+          apk mkpkg \
+            --info="name:${package_name}" \
+            --info="version:${version}" \
+            --info="arch:noarch" \
+            ${lib.concatMapStringsSep " " (x: "--info=depends:${x}") config.packages} \
+            --output "$out"
+        '';
+
+    build.depsPackageIpk =
       pkgs.runCommand "deps.ipk"
-        rec {
-          package_name = ".extra-system-deps.";
-          version = builtins.hashString "sha256" (toString config.packages);
+        {
           control = ''
             Package: ${package_name}
             Version: ${version}
+            Provides: .extra-system-deps.
+            Replaces: .extra-system-deps.
+            Conflicts: .extra-system-deps.
             Architecture: all
             Description: extra system dependencies
             ${lib.optionalString (
