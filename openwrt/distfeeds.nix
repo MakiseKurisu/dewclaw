5e3390951c5f46cbe3e519c609a2565d3de156cd
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2026-09-12 16:14:20 +0800

    fix: distfeeds was not registered

--- openwrt/distfeeds.nix
+++ openwrt/distfeeds.nix
@@ -1,16 +1,18 @@
 { ... }:
 {
-  deploySteps.distfeeds = {
-    priority = 30;
-    apply = ''
-      if command -v apk >/dev/null; then
-        apk update
-      elif command -v opkg >/dev/null; then
-        opkg update
-      else
-        echo "error: missing package manager (tried 'apk' and 'opkg')"
-        return 1
-      fi
-    '';
+  config = {
+    deploySteps.distfeeds = {
+      priority = 30;
+      apply = ''
+        if command -v apk >/dev/null; then
+          apk update
+        elif command -v opkg >/dev/null; then
+          opkg update
+        else
+          echo "error: missing package manager (tried 'apk' and 'opkg')"
+          return 1
+        fi
+      '';
+    };
   };
 }
