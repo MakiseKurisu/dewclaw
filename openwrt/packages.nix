76f7be2cd888690ddbca10630724d37a79cd0b49
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2026-09-12 14:31:25 +0800

    feat: install packages with world
    This allows us to restore some previously tested ipk support.

--- openwrt/packages.nix
+++ openwrt/packages.nix
@@ -6,25 +6,7 @@
 }:
 
 let
-  package_name = "dewclaw-deps";
-  # apk rejects hash as version number, due to the letters. Convert them to
-  # digits.
-  version = let
-    hash = builtins.hashString "sha256" (toString config.packages);
-    digitsOnly =
-      lib.stringAsChars
-        (x:
-          if x == "a" then "10"
-          else if x == "b" then "11"
-          else if x == "c" then "12"
-          else if x == "d" then "13"
-          else if x == "e" then "14"
-          else if x == "f" then "15"
-          else x
-        )
-        hash;
-    in
-      digitsOnly;
+  version = builtins.hashString "sha256" (toString config.packages);
   depsApk = config.build.depsPackageApk;
   depsIpk = config.build.depsPackageIpk;
 in
@@ -45,13 +27,6 @@ in
   };
 
   config = {
-    assertions = [
-      {
-        assertion = lib.versionAtLeast pkgs.apk-tools.version "3.0.0";
-        message = "apk-tools >= 3.0.0 is required to build APK packages (found ${pkgs.apk-tools.version}).";
-      }
-    ];
-
     deploySteps.packages = {
       priority = 80;
       copy = ''
@@ -60,14 +35,9 @@ in
       '';
       apply = ''
         if command -v apk >/dev/null; then
-          if [ "${version}" != "$(apk list --installed --manifest "${package_name}" | cut -d' ' -f2)" ]; then
-            apk update
-            # TODO: sign packages?
-            # TODO: remove old packages that were previously pulled in as dependencies, but no longer needed.
-            apk add --allow-untrusted /tmp/deps-${version}.apk
-          fi
+          sh /tmp/deps-${version}.apk
         elif command -v opkg >/dev/null; then
-          if [ "${version}" != "$(opkg info ${package_name} | grep Version | cut -d' ' -f2)" ]; then
+          if [ "${version}" != "$(opkg info ${depsIpk.package_name} | grep Version | cut -d' ' -f2)" ]; then
             opkg update
             opkg install --autoremove --force-downgrade /tmp/deps-${version}.ipk
           fi
@@ -81,30 +51,34 @@ in
     };
 
     build.depsPackageApk =
-      pkgs.runCommand "deps.apk"
-        {
-          nativeBuildInputs = [
-            pkgs.apk-tools
-          ];
-        }
-        ''
-          apk mkpkg \
-            --info="name:${package_name}" \
-            --info="version:${version}" \
-            --info="arch:noarch" \
-            ${lib.concatMapStringsSep " " (x: "--info=depends:${x}") config.packages} \
-            --output "$out"
+      pkgs.writeTextFile {
+        name = "update-world";
+        text = ''
+          #!/bin/sh
+          set -euo pipefail
+
+          if [ ! -f /etc/apk/world.dewclaw ]; then
+            echo "Missing world.dewclaw, creating it."
+            cp /etc/apk/world /etc/apk/world.dewclaw
+          fi
+
+          cp /etc/apk/world.dewclaw /etc/apk/world
+          cat <<EOF >> /etc/apk/world
+          ${lib.concatStringsSep "\n" config.packages}
+          EOF
+
+          apk update
+          apk fix --upgrade
         '';
+      };
 
     build.depsPackageIpk =
       pkgs.runCommand "deps.ipk"
-        {
+        rec {
+          package_name = ".extra-system-deps.";
           control = ''
             Package: ${package_name}
             Version: ${version}
-            Provides: .extra-system-deps.
-            Replaces: .extra-system-deps.
-            Conflicts: .extra-system-deps.
             Architecture: all
             Description: extra system dependencies
             ${lib.optionalString (
